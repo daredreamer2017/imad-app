f69cc6f46738ecb9127fb161ac64849e5da571a0
daredreamer2017 <kanan.avinash@gmail.com>
2017-07-11 22:10:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.send("Articele one requested and shown here");
+  res.send("Articele two requested and shown here");
 });
 
 app.get('/article-three', function (req, res) {
